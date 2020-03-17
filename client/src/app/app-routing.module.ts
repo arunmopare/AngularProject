4cfb2acf89eb5946b3d6ef28bdfc3751cc7e70b1
arunmopare <arunmopare546@gmail.com>
2020-03-17 16:53:11 +0530

    imroved ui

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,13 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { RegistrationFormComponent } from './registration-form/registration-form.component'
 import { ForgetPasswordComponent } from './login-form/forget-password/forget-password.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginFormComponent },
   { path: 'registration', component: RegistrationFormComponent },
-  { path: 'forgot',component:ForgetPasswordComponent}
+  { path: 'forgot',component:ForgetPasswordComponent},
+  { path: 'home',component:HomeComponent}
+
 ];
 
 @NgModule({
